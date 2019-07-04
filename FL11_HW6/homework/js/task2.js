0130dfb9afcaaae6a0fe03bc651a6259d0b671e6
alex1313111 <someone@someplace.comalex1313111@gmail.com>
2019-07-04 09:56:23 +0300

    add number check

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,16 +1,19 @@
 let a, b, c;
-a = +prompt('Input first length of triangle sides', '');
-b = +prompt('Input second length of triangle sides', '');
-c = +prompt('Input third length of triangle sides', '');
-if (a < b + c && b < a + c && c < a + b) {
-    console.log('Triangle exist');
-    if (a === b && a === c && b === c) {
-        console.log('Eequivalent triangle');
-    } else if (a === b && a !== c || a === c && a !== b || b === c && b !== a) {
-        console.log('Isosceles triangle');
+do {
+    console.log('Please enter a number');
+    a = +prompt('Input first length of triangle sides', '');
+    b = +prompt('Input second length of triangle sides', '');
+    c = +prompt('Input third length of triangle sides', '');
+  } while (isNaN(a) || isNaN(b) || isNaN(c));
+    if (a < b + c && b < a + c && c < a + b) {
+        console.log('Triangle exist');
+        if (a === b && a === c && b === c) {
+            console.log('Equivalent triangle');
+        } else if (a === b && a !== c || a === c && a !== b || b === c && b !== a) {
+            console.log('Isosceles triangle');
+        } else {
+            console.log('Normal triangle');
+        }
     } else {
-        console.log('Normal triangle');
+        console.log('Triangle doesn’t exist');
     }
-} else {
-    console.log('Triangle doesn’t exist');
-}
